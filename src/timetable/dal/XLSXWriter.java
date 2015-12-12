6ae3742413ef63b617c433503eb0d0c62704b192
samiqureshi <samiqureshi2004@gmail.com>
2015-12-12 12:40:59 +0500

    Updated License Headers

--- src/timetable/dal/XLSXWriter.java
+++ src/timetable/dal/XLSXWriter.java
@@ -1,7 +1,9 @@
- /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/*
+ * Suffa Innovation Labs - Time Table
+ *
+ * A Unified Time Table and Calendar app.
+ *
+ * SI Labs (2015)
  */
 package timetable.dal;
 import java.io.*;
